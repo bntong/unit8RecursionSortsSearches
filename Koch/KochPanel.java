f3205834eb9c6b1eaef3c206e9123a168c6ebc57
bntong <briantong05@gmail.com>
2016-04-11 16:36:12 -0500

    None
    None

--- Koch/KochPanel.java
+++ Koch/KochPanel.java
@@ -1,8 +1,3 @@
-//********************************************************************
-//  KochPanel.java       Author: Lewis/Loftus/Cocking
-//
-//  Represents a drawing surface on which to paint a Koch Snowflake.
-//********************************************************************
 
 import java.awt.*;
 import javax.swing.JPanel;
@@ -12,17 +7,8 @@ public class KochPanel extends JPanel
    private final int PANEL_WIDTH = 400;
    private final int PANEL_HEIGHT = 400;
 
-   private final double SQ = Math.sqrt(3.0) / 6;
-
-   private final int TOPX = 200, TOPY = 20;
-   private final int LEFTX = 60, LEFTY = 300;
-   private final int RIGHTX = 340, RIGHTY = 300;
-
    private int current; //current order
 
-   //-----------------------------------------------------------------
-   //  Sets the initial fractal order to the value specified.
-   //-----------------------------------------------------------------
    public KochPanel (int currentOrder)
    {
       current = currentOrder;
@@ -30,12 +16,6 @@ public class KochPanel extends JPanel
       setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
    }
 
-   //-----------------------------------------------------------------
-   //  Draws the fractal recursively. Base case is an order of 1 for
-   //  which a simple straight line is drawn. Otherwise three
-   //  intermediate points are computed, and each line segment is
-   //  drawn as a fractal.
-   //-----------------------------------------------------------------
    public void drawFractal (int order, int x1, int y1, int x5, int y5,
                             Graphics page)
    {
